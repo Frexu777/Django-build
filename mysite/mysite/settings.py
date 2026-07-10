ac23d5f1a1e13724ade28495138713011ccf54e4
Szymon <szymon@przyklad.com>
2026-07-10 23:34:39 +0200

    Dodanie brakującego pliku Dockerfile

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'postgres',
+        'PASSWORD': 'docker_haslo',
+        'HOST': 'db',  
+        'PORT': '5432',
     }
 }
 
